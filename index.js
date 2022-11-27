1d40a03d289076e0b5d56ac45652a75ebb2cf764
Josimar Henrique <josimarhenriquedeoliveira@gmail.com>
2022-11-27 13:29:15 -0300

    alternar entre as imagens +- finalizado

--- index.js
+++ index.js
@@ -2,3 +2,13 @@
 function alerta(){ 
     alert("Produto adicionado ao carrinho");
 }
+
+function img01 (){
+    document.getElementById("imagemcanecaarte").src="ImagensSublimoon/arte2.png";
+}
+function img02 (){
+    document.getElementById("imagemcanecaarte").src="ImagensSublimoon/arte3.png";
+}
+function img03 (){
+    document.getElementById("imagemcanecaarte").src="ImagensSublimoon/arte4.png";
+}
